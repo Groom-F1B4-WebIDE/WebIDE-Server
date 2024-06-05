59ac4d20f58c3701a59ce90abe52f8c1b3d31651
honge22e <20200971@sungshin.ac.kr>
2024-06-05 23:26:23 +0900

    first commit

---
+++ src/main/java/f1b4/webide_server/WebideServerApplication.java
@@ -0,0 +1,13 @@
+package f1b4.webide_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebideServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebideServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/f1b4/webide_server/WebideServerApplicationTests.java
@@ -0,0 +1,13 @@
+package f1b4.webide_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebideServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
