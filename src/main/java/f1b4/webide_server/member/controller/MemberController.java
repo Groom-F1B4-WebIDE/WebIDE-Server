7664ecd748323e610f8caeebc031f7340c611a7a
Woonig <k4327235@naver.com>
2024-06-12 17:48:05 +0900

    리파짓토리 생성/findbymemberemail기능 추가 member엔티티 매핑을 통해 db테이블 생성 입력받은 dto값을 entitiy객체로 바꿔서 사용하는 코드 추가 memberDto추가 및 entity객체는 db와 entity파일에서만 사용하기위하여 entity파일을 다시 dto파일로 변경해주는 코드 추가 memberService를 생성해서 엔티티의 tomember엔티티클래스를 통해를 통해 입력받은 dto값을 entity로 바꿔주고 리파짓토리에 save를 호출하면서 회원가입하는 코드추가 회원가입 페이지에서 post로 값이 올경우 해당 값을 modelAttribute어노테이션을 통해 Dto형태로 받아서 membrservice.save메소드 호출 완료이후 login화면 호출

--- src/main/java/f1b4/webide_server/member/controller/MemberController.java
+++ src/main/java/f1b4/webide_server/member/controller/MemberController.java
@@ -1,9 +1,12 @@
 package f1b4.webide_server.member.controller;
 
+import f1b4.webide_server.member.dto.MemberDTO;
 import f1b4.webide_server.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,6 +19,12 @@ public class MemberController {
     }
     @GetMapping("/member/login")
     public String loginForm(){
+        return "login";
+    }
+    @PostMapping("/member/save")
+    public String save(@ModelAttribute MemberDTO memberDTO)
+    {
+        memberService.save(memberDTO);
         return "login";
     }
 }
--- src/main/java/f1b4/webide_server/member/service/MemberService.java
+++ src/main/java/f1b4/webide_server/member/service/MemberService.java
@@ -1,9 +1,18 @@
 package f1b4.webide_server.member.service;
 
+import f1b4.webide_server.member.dto.MemberDTO;
+import f1b4.webide_server.member.entity.MemberEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import f1b4.webide_server.member.repository.MemberRepository;
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+    public void save(MemberDTO memberDTO) {
+        MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
+        memberRepository.save(memberEntity);
+
+    }
+
 }
